951b271e53666408ba63c76fac0133c6413905fc
asmalik107 <asmalik@outlook.com>
2024-03-03 10:41:57 +0000

    add flashlist

--- src/tabs/collapsible/index.tsx
+++ src/tabs/collapsible/index.tsx
@@ -1,7 +1,9 @@
 import {Container} from './Container';
 import {ScrollView} from './ScrollView';
+import {FlatList} from './FlatList';
 
 export const Tabs = {
   Container,
+  FlatList,
   ScrollView,
 };
