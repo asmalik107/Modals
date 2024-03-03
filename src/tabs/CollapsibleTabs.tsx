977e47227defed189ebe7e94215f9d5221b69bfd
asmalik107 <asmalik@outlook.com>
2024-03-03 10:20:55 +0000

    cards for collapsible tabs

--- src/tabs/CollapsibleTabs.tsx
+++ src/tabs/CollapsibleTabs.tsx
@@ -1,6 +1,61 @@
 import {Route} from 'react-native-tab-view';
 import {Tabs} from './collapsible';
 import {FirstRoute, Header, SecondRoute, ThirdRoute} from './TabViews';
+import {StyleSheet, Text, View} from 'react-native';
+import {FC} from 'react';
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    padding: 16,
+    marginHorizontal: 16,
+    borderColor: 'white',
+    marginVertical: 10,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+  },
+});
+
+type TestData = {
+  id: number;
+  title: string;
+  subtitle: string;
+};
+
+const generateObjectList = (count: number): TestData[] => {
+  return Array.from({length: count}, (_, index) => ({
+    id: index + 1,
+    title: `Title ${index + 1}`,
+    subtitle: `SubTitle ${index + 1}`,
+  }));
+};
+
+// Example: Generate a list of 5 objects
+const data: TestData[] = generateObjectList(20);
+
+const Card: FC<TestData> = ({title, subtitle}) => {
+  return (
+    <View accessible style={styles.card}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{subtitle}</Text>
+    </View>
+  );
+};
 
 export const renderTabScene = (route: Route) => {
   let renderItem;
@@ -8,7 +63,11 @@ export const renderTabScene = (route: Route) => {
     case 'first':
       renderItem = (
         <Tabs.ScrollView sceneKey={route.key}>
-          <FirstRoute />
+          <>
+            {data.map(item => (
+              <Card key={item.id} {...item} />
+            ))}
+          </>
         </Tabs.ScrollView>
       );
       break;
